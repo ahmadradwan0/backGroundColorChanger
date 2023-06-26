13c903c14c51d286849cb8179f7a906107074d8d
ahmadradwan0 <ahmad@ahmadradwan.dev>
2023-06-25 19:51:39 -0500

    adding a reverse color To the H1

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,36 @@
 $(document).ready(function () {
-    $("#color-range").on('input', function () {
-        $("body").css("background-color", $(this).val());
+    var colorRangeInput = $("#color-range");
+    var body = $("body");
+    var backgroundColor = "background-color";
+    
+    
+    $(colorRangeInput).on('input',function(){
+        body.css(backgroundColor,colorRangeInput.val());
+        $("#colorText").css("color",reverseColor(colorRangeInput.val()))
+        
     });
+
+
+    function reverseColor(color) {
+        // Remove the leading #
+        color = color.slice(1);
+        
+        // Convert to RGB
+        var red = parseInt(color.slice(0, 2), 16);
+        var green = parseInt(color.slice(2, 4), 16);
+        var blue = parseInt(color.slice(4, 6), 16);
+        
+        // Reverse the RGB values
+        red = 255 - red;
+        green = 255 - green;
+        blue = 255 - blue;
+        
+        // Convert back to hexadecimal color code
+        var reversedColor = '#' + (red.toString(16)).padStart(2, '0') +
+                            (green.toString(16)).padStart(2, '0') +
+                            (blue.toString(16)).padStart(2, '0');
+        
+        return reversedColor;
+      }
+    
 });
